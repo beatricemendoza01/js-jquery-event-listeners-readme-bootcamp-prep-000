211ae49490fc94c6421af4040f5bf571a4517f57
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-29 20:38:06 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ function frameIt() {
 function pressIt() {
   $('#typing').on('keydown', function(key) {
     if (key.which === 71) {
-      
+      alert('You have pressed the letter g');
     }
   });
 }
